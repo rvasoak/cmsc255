7dba023b1c2a879287ca9d574f5182b991b9a4e3
rvasoak <46756616+rvasoak@users.noreply.github.com>
2019-01-16 11:56:59 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Prince Sebastian");
+		System.out.println("Hello.java");
+		System.out.println("Java SDK Setup");
+		System.out.println("SE 8u201");
+		System.out.println("CMSC 255 004");
+		System.out.println();
+	}
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+		
+		
